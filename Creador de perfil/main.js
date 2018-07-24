43a671f6eef1f8ca0b84956d555cc510e99169d7
Luso233 <luisquintero2001@gmail.com>
2018-07-23 21:31:54 -0500

    Agregado starter template y maquetado basico

---
+++ Creador de perfil/main.js
@@ -0,0 +1,6 @@
+let Canvas = () => {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("scream");
+    ctx.drawImage(img,10,10);
+}
