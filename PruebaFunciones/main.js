c7dc1d0a24f7cca4d25fb1a731d8e07eddf97a90
Luso233 <luisquintero2001@gmail.com>
2018-07-24 20:28:49 -0500

    Arreglado el flexbox y agregado el fondo en la pestaña del puerco

---
+++ PruebaFunciones/main.js
@@ -0,0 +1,10 @@
+let Dado = document.getElementById("Dado");
+let Masa = document.getElementById ("masa");
+let Altura = document.getElementById ("Altura");
+let total = 0
+let IMC = (n1,n2) =>{
+Dado.addEventListener ("click",() =>{
+	console.log ("Si hizo click")
+	total=Number(Masa.value)  Number(Altura.value)
+	resultado.innerHTML = total;
+}
