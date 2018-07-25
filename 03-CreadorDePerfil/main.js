5538f88d98d42719186117525d72d46d853a63ab
Luso233 <luisquintero2001@gmail.com>
2018-07-24 21:40:52 -0500

    Agregada la funcion de IMC y arreglado el pinche flexbox del canvas :DDDDDD

--- 03-CreadorDePerfil/main.js
+++ 03-CreadorDePerfil/main.js
@@ -1,6 +1,17 @@
-let Canvas = () => {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("scream");
-    ctx.drawImage(img,10,10);
-}
+
+// let Canvas = () => {
+//     var c = document.getElementById("myCanvas");
+//     var ctx = c.getContext("2d");
+//     var img = document.getElementById("scream");
+//     ctx.drawImage(img,10,10);
+// }
+let resultado = document.getElementById("resultado");
+let Dado = document.getElementById("dado");
+let Masa = document.getElementById ("masa");
+let Altura = document.getElementById ("altura");
+let total = 0;
+Dado.addEventListener ("click",() =>{
+	console.log ("Si hizo click")
+	total=Number(Masa.value)/Number((Altura.value)*(Altura.value))
+	resultado.innerHTML = total;
+})
