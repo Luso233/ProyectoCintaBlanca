7c02fe10ac4a4460a269c4aa1b68816cdb660335
Luso233 <luisquintero2001@gmail.com>
2018-07-25 21:31:41 -0500

    Agregadas las funciones del canvas, Asignacion de variables y arreglado el boton de la pagina 3

--- 03-CreadorDePerfil/main.js
+++ 03-CreadorDePerfil/main.js
@@ -1,11 +1,4 @@
 
-// let Canvas = () => {
-//     var c = document.getElementById("myCanvas");
-//     var ctx = c.getContext("2d");
-//     var img = document.getElementById("scream");
-//     ctx.drawImage(img,10,10);
-// }
-let resultado = document.getElementById("resultado");
 let Dado = document.getElementById("dado");
 let Masa = document.getElementById ("masa");
 let Altura = document.getElementById ("altura");
@@ -13,5 +6,21 @@ let total = 0;
 Dado.addEventListener ("click",() =>{
 	console.log ("Si hizo click")
 	total=Number(Masa.value)/Number((Altura.value)*(Altura.value))
-	resultado.innerHTML = total;
+if (total >=18 && total<=25) {
+	window.location.replace("");
+}else if (total >=26 && total<=30){
+	window.location.replace("");
+}else if (total >=31){
+	window.location.replace("");
+}else if (total <18){
+	window.location.replace("");
+}
 })
+
+let MiCanvas() {
+    let c = document.getElementById("miCanvas");
+    let ctx = c.getContext("2d");
+    let silueta1 = img.src (guerrera.png);
+    let silueta2 = img.src ()
+i    ctx.drawImage(img,10,10);
+}
