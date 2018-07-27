eb04889f481d759d8e1d8cfacc57ee6e738c5340
PuggoIG <ibgayael@live.com>
2018-07-27 17:33:21 -0500

    Se actualizaron mision-1.html y rutinas.js, cambia de nombre a 04-rutinas.js

--- 04-Rutinas/rutinas.js
+++ 04-Rutinas/rutinas.js
@@ -20,10 +20,48 @@
 //   } else {
 //     console.log("Sin checar");
 //   }
-let checkbox = document.getElementById("Lunes");
+let checkbox1 = document.getElementById("Lunes");
+let checkbox2 = document.getElementById("Martes");
+let checkbox3 = document.getElementById("Miércoles");
+let checkbox4 = document.getElementById("Jueves");
+let checkbox5 = document.getElementById("Viernes");
 
-checkbox.addEventListener("click", function() {
-    if(checkbox.checked) {
+
+checkbox1.addEventListener("click", function()
+ {
+    if(checkbox1.checked && checkbox2.checked && checkbox3.checked && checkbox4.checked && checkbox5.checked) {
+       console.log("checado");
+    } else {
+        console.log("Sin checar");
+    }
+});
+checkbox2.addEventListener("click", function()
+	{
+    if(checkbox1.checked && checkbox2.checked && checkbox3.checked && checkbox4.checked && checkbox5.checked) {
+       console.log("checado");
+    } else {
+        console.log("Sin checar");
+    }
+});
+checkbox3.addEventListener("click", function()
+	{
+    if(checkbox1.checked && checkbox2.checked && checkbox3.checked && checkbox4.checked && checkbox5.checked) {
+       console.log("checado");
+    } else {
+        console.log("Sin checar");
+    }
+});
+checkbox4.addEventListener("click", function()
+	{
+    if(checkbox1.checked && checkbox2.checked && checkbox3.checked && checkbox4.checked && checkbox5.checked) {
+       console.log("checado");
+    } else {
+        console.log("Sin checar");
+    }
+});
+checkbox5.addEventListener("click", function()
+{
+	if(checkbox1.checked && checkbox2.checked && checkbox3.checked && checkbox4.checked && checkbox5.checked) {
        console.log("checado");
     } else {
         console.log("Sin checar");
